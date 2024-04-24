91eae6ae5ed1745c054b6961680f5d82b2534e8a
WanjaMaryanne <maryannewanja4@gmail.com>
2024-04-24 22:51:18 +0300

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,21 +4,3 @@ package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
-
-# Check if Flask is installed correctly
-exec { 'check_flask_version':
-  command   => 'flask --version | grep "Flask 2.1.0"',
-  path      => '/usr/local/bin:/usr/bin:/bin',
-  logoutput => true,
-  subscribe => Package['Flask'],
-  refreshonly => true,
-}
-
-#prints success message
-exec { 'print_success_message':
-  command     => 'echo "Your Puppet manifest configures the host according to requirements"',
-  path        => '/usr/local/bin:/usr/bin:/bin',
-  logoutput   => true,
-  subscribe   => Exec['check_flask_version'],
-  refreshonly => true,
-}
