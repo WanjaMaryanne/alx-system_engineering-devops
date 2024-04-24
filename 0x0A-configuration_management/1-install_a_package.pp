a82fc9320f1b126c4f1e9a7285d7ca46ea0d105b
WanjaMaryanne <maryannewanja4@gmail.com>
2024-04-24 23:20:32 +0300

    code fix for Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
-#!/usr/bin/env puppet
-#install flask from pip3
+#!/usr/bin/pup
+#install a specific version of flask
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-}
+  provider => 'pip',
+ }
 
